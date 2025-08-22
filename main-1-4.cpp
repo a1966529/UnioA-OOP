a3c82db5e6eef5cf6d81e394f51876727df9bc32
a1966529 <a1966529@adelaide.edu.au>
2025-08-22 10:52:12 +0930

    .

--- main-1-4.cpp
+++ main-1-4.cpp
@@ -2,18 +2,18 @@
 #include "Person.h"
 
 extern PersonList shallowCopyPersonlist(PersonList PL);
-extern PersonList createPersonList(int n);
+// extern PersonList createPersonList(int n);
 
-int main()
-{
-    int size = 5;
-    PersonList PL_1 = createPersonList(size); 
-    PersonList PL = shallowCopyPersonlist(PL_1); 
-    Person* ptr = PL.people;
-        std::cout << "NumberPeople: " << PL.numPeople << std::endl;
-        for (int i = 0; i < size; i++)
-    {
-        std::cout << "Person " << i + 1 << ": " << (ptr + i)->name << ", " << (ptr + i)->age << std::endl;
-    }
-    return 0;
-}
+// int main()
+// {
+//     int size = 5;
+//     PersonList PL_1 = createPersonList(size); 
+//     PersonList PL = shallowCopyPersonlist(PL_1); 
+//     Person* ptr = PL.people;
+//         std::cout << "NumberPeople: " << PL.numPeople << std::endl;
+//         for (int i = 0; i < size; i++)
+//     {
+//         std::cout << "Person " << i + 1 << ": " << (ptr + i)->name << ", " << (ptr + i)->age << std::endl;
+//     }
+//     return 0;
+// }
